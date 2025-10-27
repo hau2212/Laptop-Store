68f3dc47f32c72efbc513295497fc6f293c1d7aa
Khangcyber <khangnt24@saigontech.edu.vn>
2025-10-27 14:52:33 +0700

    comit 2:52 nhanh hoan thien

--- Laptop_Store/routes/web.php
+++ Laptop_Store/routes/web.php
@@ -13,6 +13,8 @@ Route::get('/admin' , 'App\Http\Controllers\Admin\AdminController@index')->name(
 Route::get('/admin/products' , 'App\Http\Controllers\Admin\AdminController@createProducts')->name('admin.products');
 Route::post('/admin/product/store' , 'App\Http\Controllers\Admin\AdminController@store')->name('admin.product.store');
 Route::delete('/admin/product/delete/{id}' , 'App\Http\Controllers\Admin\AdminController@delete')->name('admin.product.delete');
+
+
 Route::get('/admin/product/edit/{id}', 'App\Http\Controllers\admin\AdminController@edit')->name('admin.product.edit');
 Route::put('/admin/product/update/{id}', 'App\Http\Controllers\admin\AdminController@update')->name('admin.product.update');
 
