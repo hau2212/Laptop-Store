7164485b39d83fef3a5d9bc83a922799d2969ee1
Khangcyber <khangnt24@saigontech.edu.vn>
2025-10-29 21:12:57 +0700

    gan hoan thien 29/10: 9h12p

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,6 +55,7 @@
                     </datalist>        
                 </div>
 
+                
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNav"
                 aria-controls="mainNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
